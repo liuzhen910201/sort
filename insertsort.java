377c19494da5c2185bf07d2c774d476b0d21acb0
liuzhen910201 <lz910201@gmail.com>
2017-06-23 16:23:12 +0900

    insertsort

--- insertsort.java
+++ insertsort.java
@@ -1,23 +1,22 @@
 public class insertsort{
 	public static void main(String[] args){
-		int[] list={1,3,4,2,9,5,6,10,8};
+		int[] list={11,3,4,7,2,9,5,6,10,8,1};
 		insert(list,1,1);
 		for(int n=0;n<list.length;n++)
 			System.out.println(list[n]);
 	}
 	public static void insert(int[] list, int x, int y){
 		int temp;
-		if(y>0){
-			if (list[y]<list[y-1]){
+		if((y>=0)&&(x<=list.length)){
+			if (y>0&&(list[y]<list[y-1])){
 				temp=list[y];
 				list[y]=list[y-1];
 				list[y-1]=temp;
+                insert(list,x,y-1);
 			}
-			else if(x<list.length){//放在这里更高效
+			else{
 				insert(list,x+1,x);
-			}	
-			insert(list,x,y-1);
-			//放在这里低效
+			}
 		}	
 	}
 }
